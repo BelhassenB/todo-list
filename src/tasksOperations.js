14da316c11e5fd8035e5a5f6a71171360a488ec3
Belhassen <152809495+BelhassenB@users.noreply.github.com>
2024-06-19 17:12:43 +0200

    Add functions to get tasks values and submit edited task

--- src/tasksOperations.js
+++ src/tasksOperations.js
@@ -1,5 +1,8 @@
 import { projectsManagement } from "./app";
-import { main, toDo, dialog } from "./userInterface";
+import { main, toDo, submitFormBtn ,dialog, getTaskValues } from "./userInterface";
+// import { title, description, dueDate, priority, form } from "./userInterface";
+
+const submitEditForm = document.querySelector('.submitEditTask')
 
 export function manageTask() {   
     
@@ -9,12 +12,16 @@ export function manageTask() {
     let svgContainer = e.target.parentElement // get the parent element of svgs
     let taskContainer = svgContainer.parentElement // get taskref of the container
     let taskContainerRef =  taskContainer.dataset.taskref    
+    let taskToEdit = toDo.inbox[taskContainerRef]
     
     if (element.className === 'delete-svg') {
         toDo.deleteTask(taskContainerRef)
         removeTaskFromDOM(taskContainer)
     } else if (element.className === 'edit-svg') {
-        console.log('test')
+        displayFormToEditTask(taskToEdit)
+        saveEditedTask(taskContainerRef)
+        
+
 
     } else console.log(toDo.inbox)
    })
@@ -23,6 +30,26 @@ export function manageTask() {
 function removeTaskFromDOM(taskToRemove) {
     main.removeChild(taskToRemove)
 }
-function editTaskfromDOM() {
-
+// display form to edit task and popualte it with
+// values  of the task details
+function displayFormToEditTask(selectedTask) {
+    const currentValue = getTaskValues()
+    
+    submitEditForm.removeAttribute("hidden")
+        submitFormBtn.replaceWith(submitEditForm)
+        currentValue.title.value = selectedTask.title
+        currentValue.description.value = selectedTask.description
+        currentValue.priority.value = selectedTask.priority
+        currentValue.dueDate.value = selectedTask.dueDate
+        dialog.showModal()
+}
+function saveEditedTask(selectedTask) {
+    submitEditForm.addEventListener("click", () => {
+        toDo.editTask(selectedTask,
+            title.value,
+            description.value,
+            dueDate.value,
+            priority.value
+        )
+    })
 }
