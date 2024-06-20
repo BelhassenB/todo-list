1054fa1053286cb628e361cb56ed69145c164508
Belhassen <152809495+BelhassenB@users.noreply.github.com>
2024-06-20 13:10:22 +0200

    Divide newTaskContainer function into 3 different parts

--- src/userInterface.js
+++ src/userInterface.js
@@ -3,61 +3,81 @@ import EditTask from './img/edit-task.svg'
 import DeleteTask from './img/delete-task.svg'
 import MoveTask from './img/move-task.svg'
 
-const body = document.querySelector('body')
-const main = document.querySelector('.main')
+export const main = document.querySelector('.main')
+export const dialog = document.querySelector('dialog')
+export const submitFormBtn = document.querySelector('.submit')
+export const title = document.querySelector('#title')
+export const description = document.querySelector('#description')
+export const dueDate= document.querySelector('#dueDate')
+export const priority = document.querySelector('#priority')
+export const form = document.querySelector('form')
 const addTaskBtn = document.querySelector('.addTask')
-const dialog = document.querySelector('dialog')
-const submitFormBtn = document.querySelector('.submit')
-const title = document.querySelector('#title')
-const description = document.querySelector('#description')
-const dueDate= document.querySelector('#dueDate')
-const priority = document.querySelector('#priority')
-const form = document.querySelector('form')
 
-const toDo = projectsManagement()
+
+export const toDo = projectsManagement()
 
 export function displayTaskForm() {  
     addTaskBtn.addEventListener("click", () => {
+        form.reset()
         dialog.showModal()
     })
     
 }
-export function displayTaskOnClick() {
+export function createNewTask() {
+    toDo.createTask(title.value, description.value, dueDate.value, priority.value)
+    form.reset()
+}
+export function displayTaskOnClick() {   
     submitFormBtn.addEventListener("click", () => {
-        const taskCreated = toDo.createTask(title.value, description.value, dueDate.value, priority.value)
-        displayInboxTasks(taskCreated)
-        form.reset()
+        createNewTask()
+        // main.replaceChildren() // emtpy the main div to avoid duplicate
+        createTaskContainer(toDo.inbox)
 
     })
 }
-function displayInboxTasks(task) {
-    const newContainer = document.createElement('div')
-    
-    for (const property in task) {
-        const taskDetail = document.createElement('div')
-        taskDetail.style.fontWeight = "bold"
-        taskDetail.textContent = task[property]
-        newContainer.append(taskDetail)
+export function createTaskContainer(taskToDisplay) {
+        main.replaceChildren()
+    for (let i=0; i<taskToDisplay.length; i++) {
+        const newContainer = document.createElement('div')
+        newContainer.dataset.taskref = (toDo.inbox).indexOf(taskToDisplay[i])      
+       
+        main.append(newContainer) 
+
+        for (const property in taskToDisplay[i]) {
+            const taskDetail = document.createElement('div')
+            taskDetail.style.fontWeight = "bold"
+            taskDetail.textContent = taskToDisplay[i][property]
+            newContainer.append(taskDetail) 
+        }
+
+        const iconsDiv = document.createElement('div')
+        iconsDiv.classList.add('svgDiv')
+        iconsDiv.style.padding = "10px"
+        iconsDiv.style.display = "flex"
+        iconsDiv.style.justifyContent = "space-around"
+        newContainer.append(iconsDiv)
+
+        const editSvg = new Image(30,30)
+        editSvg.src = EditTask
+        editSvg.classList.add('edit-svg')
+
+        const moveSvg = new Image(30,30)
+        moveSvg.src = MoveTask
+        moveSvg.classList.add('move-svg')
+
+        const deleteSvg = new Image(30,30)
+        deleteSvg.src = DeleteTask
+        deleteSvg.classList.add('delete-svg')
+
+        iconsDiv.append(editSvg, moveSvg, deleteSvg)
     }
-    const manageTask = document.createElement('div')
-    manageTask.classList.add('svgDiv')
-    manageTask.style.padding = "10px"
-    manageTask.style.display = "flex"
-    manageTask.style.justifyContent = "space-around"
-    const editSvg = new Image(30,30)
-    editSvg.src = EditTask
-    editSvg.classList.add('editSvg-btn')
-    const moveSvg = new Image(30,30)
-    moveSvg.src = MoveTask
-    moveSvg.classList.add('moveSvg-btn')
-    const deleteSvg = new Image(30,30)
-    deleteSvg.src = DeleteTask
-    deleteSvg.classList.add('deleteSvg-btn')
-
-    manageTask.append(editSvg)
-    manageTask.append(moveSvg)
-    manageTask.append(deleteSvg)
-
-    newContainer.append(manageTask)
-    main.append(newContainer)
+}
+export function getTaskValues() {
+    return {
+        title,
+        description,
+        dueDate,
+        priority,
+        form
+    }
 }
