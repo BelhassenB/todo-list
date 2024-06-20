8e22c59d3cd119b6d25e020c9ac13226c84e18a9
Belhassen <152809495+BelhassenB@users.noreply.github.com>
2024-06-20 13:12:22 +0200

    Add functions to edit tasks

--- src/tasksOperations.js
+++ src/tasksOperations.js
@@ -1,49 +1,59 @@
 import { projectsManagement } from "./app";
-import { main, toDo, submitFormBtn ,dialog, getTaskValues } from "./userInterface";
-// import { title, description, dueDate, priority, form } from "./userInterface";
+import { main, toDo, submitFormBtn ,dialog, getTaskValues, createTaskContainer } from "./userInterface";
+
 
 const submitEditForm = document.querySelector('.submitEditTask')
 
 export function manageTask() {   
     
-   main.addEventListener("click", (e) => {
+   main.addEventListener("click", (e) => {       
+    const element = e.target 
+    const svgContainer = e.target.parentElement // get the parent element of svgs
+    const taskContainer = svgContainer.parentElement // get taskref of the container
+    const taskContainerRef =  taskContainer.dataset.taskref    
+    const taskToEdit = toDo.inbox[taskContainerRef]   
     
-    let element = e.target 
-    let svgContainer = e.target.parentElement // get the parent element of svgs
-    let taskContainer = svgContainer.parentElement // get taskref of the container
-    let taskContainerRef =  taskContainer.dataset.taskref    
-    let taskToEdit = toDo.inbox[taskContainerRef]
-    
+
     if (element.className === 'delete-svg') {
         toDo.deleteTask(taskContainerRef)
         removeTaskFromDOM(taskContainer)
     } else if (element.className === 'edit-svg') {
         displayFormToEditTask(taskToEdit)
-        saveEditedTask(taskContainerRef)
-        
-
-
+        showEditBtnOnForm()
+        saveEditedTask(taskContainerRef)       
+  
     } else console.log(toDo.inbox)
    })
 }
 
 function removeTaskFromDOM(taskToRemove) {
     main.removeChild(taskToRemove)
+    createTaskContainer(toDo.inbox) // to reset to tak ref according to inbox positions
+
 }
+
 // display form to edit task and popualte it with
 // values  of the task details
 function displayFormToEditTask(selectedTask) {
     const currentValue = getTaskValues()
     
-    submitEditForm.removeAttribute("hidden")
-        submitFormBtn.replaceWith(submitEditForm)
-        currentValue.title.value = selectedTask.title
-        currentValue.description.value = selectedTask.description
-        currentValue.priority.value = selectedTask.priority
-        currentValue.dueDate.value = selectedTask.dueDate
-        dialog.showModal()
+    currentValue.title.value = selectedTask.title
+    currentValue.description.value = selectedTask.description
+    currentValue.priority.value = selectedTask.priority
+    currentValue.dueDate.value = selectedTask.dueDate
+    dialog.showModal()
 }
+function showEditBtnOnForm() {
+    submitEditForm.removeAttribute("hidden")
+    submitFormBtn.replaceWith(submitEditForm)
+}
+function hideEditBtnOnForm() {
+    submitEditForm.setAttribute("hidden", true)
+    submitEditForm.replaceWith(submitFormBtn)
+}
+
 function saveEditedTask(selectedTask) {
+    
     submitEditForm.addEventListener("click", () => {
         toDo.editTask(selectedTask,
             title.value,
@@ -51,5 +61,9 @@ function saveEditedTask(selectedTask) {
             dueDate.value,
             priority.value
         )
+        hideEditBtnOnForm()
+        createTaskContainer(toDo.inbox)
+        dialog.close()
+
     })
 }
