6de34c4d787fb9a6e40b1b682b9177cca1a8a956
Belhassen <152809495+BelhassenB@users.noreply.github.com>
2024-06-25 11:48:22 +0200

    Add function to display projects

--- src/projects.js
+++ src/projects.js
@@ -3,61 +3,76 @@ const displayProjectFormbtn = document.querySelector('.displayProjectForm')
 const projectsContainer = document.querySelector('.projects-container')
 const projectTarget = document.querySelector('.projects-container') // to use event delegation
 
-
 function createProjectForm() {   
     const projectDialog = document.createElement('dialog')
-    projectDialog.classList.add('dialog-display')
-    
+    projectDialog.classList.add('dialog-display')  
+
     const projectForm = document.createElement('form')
-    // projectForm.method = 'dialog'
-    // projectForm.action = ""
-    projectForm.classList.add('project-form')
-    
+    projectForm.method = 'dialog'
+    projectForm.action = " "
+    projectForm.classList.add('project-form')  
+    projectForm.id = "formId"        
 
     const projectNameInput = document.createElement('input')
     projectNameInput.id = 'projectName'    
     projectNameInput.type = 'text'
-    projectNameInput.placeholder = ' Enter a name for your project'
+    projectNameInput.placeholder = ' Enter a name for your project'   
     
 
     const submitProjectName = document.createElement('button')
     submitProjectName.type = 'submit'    
     submitProjectName.textContent = 'Add'
     submitProjectName.classList.add('submitProject')
-    
+    submitProjectName.for = "formId"    
 
     const closeFormbtn = document.createElement('button')
     closeFormbtn.textContent = 'Close'
-
+    closeFormbtn.type = 'button'
+    closeFormbtn.classList.add('closeForm')
     
     projectForm.append(projectNameInput, submitProjectName, closeFormbtn)
     projectDialog.append(projectForm)
-    projectsContainer.append(projectDialog)
 
-    projectDialog.show()
-    
+    return projectDialog    
     
 }
+const projectDialog = createProjectForm()
+
 export function displayProjectFormOnClick() {
     displayProjectFormbtn.addEventListener("click", () => {
-        createProjectForm() 
-        //hide button to avoid user display another form       
-        displayProjectFormbtn.setAttribute("hidden", true)
+        projectsContainer.replaceChildren()
+        projectsContainer.appendChild(projectDialog)
+        projectDialog.showModal() 
 
     })
 }
 
-export function submitNewProject() {
-   
+export function submitNewProject() {       
     projectTarget.addEventListener("click", (e) => {
         let btnTarget = e.target
-        if (btnTarget.className === "submitProject") {
-            const inputValue = document.querySelector('#projectName')
-            toDo.createProject(inputValue.value)
-            const selectDialog = document.querySelector('.dialog-display')
+        const selectDialog = document.querySelector('.dialog-display')         
+        const selectForm = document.querySelector('.project-form')
+        const inputValue = document.querySelector('#projectName')
+        if (btnTarget.className === "submitProject") {         
+            toDo.createProject(inputValue.value)                     
+            selectForm.reset()
             selectDialog.close()
-            displayProjectFormbtn.removeAttribute("hidden")
+            displayProjectsOnDOM()
 
-        }
+        } else if (btnTarget.className === "closeForm") {
+            selectDialog.close()    
+        }        
+        
     })
+}
+function displayProjectsOnDOM() {
+    let listOfProjects = toDo.projects
+    for (let i=0; i<listOfProjects.length; i++) {
+        const projectDiv = document.createElement('div')
+        projectDiv.textContent = listOfProjects[i].name
+        projectDiv.dataset.projectref = listOfProjects.indexOf(listOfProjects[i])
+        projectDiv.style.marginBottom = "20px"
+        projectsContainer.appendChild(projectDiv)
+        
+    }
 }
