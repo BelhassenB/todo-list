e0d1bbd961b3f44a7d61bd7f7769eaba2fed0e8b
Belhassen <152809495+BelhassenB@users.noreply.github.com>
2024-06-30 20:25:18 +0100

    Update function to save data when page is closed/refreshed

--- src/storage.js
+++ src/storage.js
@@ -1,17 +1,44 @@
-import { toDo, main, createTaskContainer } from "./userInterface"
+import { toDo, createTaskContainer } from "./userInterface"
+import { displayProjectNameOnDOM } from "./projects"
 
 
-export function storeUserTasks() {
-    localStorage.setItem("userInbox", JSON.stringify(toDo.inbox) )
-    
+function storeUserTasks() {      
+    localStorage.setItem("userTasks", JSON.stringify(toDo.inbox))
 }
-export function storeUserProejcts() {
+function storeUserProjects() {
     localStorage.setItem("userProjects", JSON.stringify(toDo.projects))
 }
+export function storeData() {
+    window.addEventListener("beforeunload", () => {
+        storeUserTasks()
+        storeUserProjects()
+    })
+}
 
-export function populateData() {
+export function populateTasks () {
+   document.addEventListener("DOMContentLoaded", () => {
+    const getTasksLocalStorage = localStorage.getItem("userTasks")
+        
+
+    if (getTasksLocalStorage) {
+        const parseUserTasks = JSON.parse(getTasksLocalStorage)
+        for (let i=0; i<parseUserTasks.length; i++) {
+            toDo.inbox.push(parseUserTasks[i])
+            createTaskContainer(toDo.inbox)
+        }
+    }
+    })
+}
+export function populateProjects() {
     document.addEventListener("DOMContentLoaded", () => {
-        const parsedData = JSON.parse(localStorage.getItem("userInbox"))
-        createTaskContainer(parsedData)
-    })    
+        const getProjectFromLocalStorage = localStorage.getItem("userProjects")        
+
+        if (getProjectFromLocalStorage) {
+            const parseUserProjects = JSON.parse(getProjectFromLocalStorage)  
+            for (let i=0; i<parseUserProjects.length; i++) {
+                toDo.projects.push(parseUserProjects[i])
+                displayProjectNameOnDOM()
+            }
+        }
+    })
 }
